4619619ffd60569ff7aeef0fdd3a8f9879dbbfdc
Kellen Green <me@kellengreen.com>
2017-10-08 12:13:26 +0100

    Update hammerspoon.lua

--- osx/hammerspoon.lua
+++ osx/hammerspoon.lua
@@ -5,16 +5,16 @@ function pressKey(key)
 end
 
 function goFullScreen()
+    local offset = 4
     local window = hs.window.focusedWindow()
     local frame = window:frame()
     local screen = window:screen():frame()
-    frame.x = screen.x
+    frame.x = screen.x - offset
     frame.y = screen.y
-    frame.w = screen.w
+    frame.w = screen.w + offset
     frame.h = screen.h
     window:setFrame(frame)
 end
 
 hs.hotkey.bind({'alt'}, 'delete', nil, pressKey('forwarddelete'), pressKey('forwarddelete'))
-hs.hotkey.bind({'alt'}, '`', nil, pressKey('escape'), pressKey('escape'))
 hs.hotkey.bind({'alt'}, 'f', goFullScreen)
